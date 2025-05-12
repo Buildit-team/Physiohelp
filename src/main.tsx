7d6b40035f48459acc1833bac79dece8a6446444
youngmentor <olaleresuliton43@gmail.com>
2025-05-12 09:45:55 +0100

    fix(main): comment out Analytics import and usage for future reference

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import App from './App.tsx'
 import { CartProvider } from './context/CartCaontex.tsx'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { AdminProvider } from './context/adminContext.tsx'
-import { Analytics } from "@vercel/analytics/next"
+// import { Analytics } from "@vercel/analytics/next"
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <CartProvider>
       <AdminProvider>
         <App />
-        <Analytics />
+        {/* <Analytics /> */}
       </AdminProvider>
     </CartProvider>
   </QueryClientProvider>
